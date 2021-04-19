32a5f6aa77ecb2121a54e1dae3ac7c40002e2af0
Sam Sheets <sam.sheets94@gmail.com>
2021-04-19 15:45:26 -0600

    implement Redux for Posts components

--- src/App.js
+++ src/App.js
@@ -2,29 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import Posts from './Components/Posts'
 import PostForm from './Components/PostForm';
+import {Provider} from 'react-redux';
+import Store from './Store'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-
-    </div>
+    <Provider store={Store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
